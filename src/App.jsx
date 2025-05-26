8f271f8ee9b1f404ff0ddf28ef4eb8a6e062c5c7
Ravi Varma Lingala <ravivarma560@gmail.com>
2025-05-26 12:47:32 -0400

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import {Box, Container, useToast} from "@chakra-ui/react";
+import Navbar from "./components/layout/Navbar";
+import {Canvas} from "./Canvas";
+import ColorPalette from "./components/layout/ColorPalette";
+import Header from "./components/layout/Header";
+import {useEffect, useState} from "react";
+import PortfolioList from "./components/layout/portfolio/PortfolioList";
+import Contact from "./components/layout/Contact";
+import Copyright from "./components/layout/Copyright";
+import {motion} from "framer-motion";
+import Experience from "./components/layout/exp/Experience";
+
+function App() {
+  return (
+    <div>
+      <Container maxW='1300px'>
+        <Navbar />
+        <Header />
+        <Experience />
+        <PortfolioList />
+        <Contact />
+        <Copyright />
+      </Container>
+      <Canvas />
+      <ColorPalette />
+    </div>
+  );
+}
+
+export default App;
